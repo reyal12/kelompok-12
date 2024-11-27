38bd0082ec7d8bd04b7532b5942411991ed17281
reyal12 <rheal.rir@gmail.com>
2024-11-27 19:11:16 +0700

    Update Kelompok12.cpp

--- Kelompok12.cpp
+++ Kelompok12.cpp
@@ -27,3 +27,30 @@ int main(){
 	mvprintw (16, 55, "      ");
 	mvprintw (16, 54, "|          |");
 		refresh();
+	
+		for (int f=0; f<10; f++){
+			start_color();
+			init_pair(1,COLOR_WHITE,COLOR_WHITE);
+			attron(COLOR_PAIR(1));
+				mvprintw(16, 55 + f, ">");
+			refresh();
+			Sleep(600);
+			attroff(COLOR_PAIR(1));
+	}
+	clear();
+	
+	mvprintw (15, 55, "Login Berhasil!");
+		refresh();
+		Sleep(1000);
+		clear();
+	
+	mvprintw (12, 55, "Main Menu");
+	mvprintw (13, 55, "1. Start");
+	mvprintw (14, 55, "2. Option");
+	mvprintw (15, 55, "3. Keluar");
+	mvprintw (16, 55, "4. Pilih Menu :");
+		refresh();
+	
+	move(16, 70);
+	echo();
+	pilihan = getch();
